64be11a46c6435e3200ec4b17a152ac69b585276
Zizou <abdul.fuseini@ashesi.edu.gh>
2021-09-22 00:00:13 +0000

    update

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,54 +1,27 @@
 #include <stdio.h>
 #include <string.h>
-#define MAXLINE 10000
+#include <stdlib.h>
 
 
+int main(int argc, char *argv[]){
 
+    char line[1024];
+    //char *temp = NULL;
+    char *tofind = NULL;
+    tofind = argv[1];
 
-int get_line(char *line, int max);
+    FILE *fp = fopen(argv[2], "r");
+    if (fp == NULL)
+    {
+        printf("cannot open file \n");
+        exit(1);
+    }
 
-
-/* find: print lines that match pattern from 1st arg */
-
-
-int main(int argc, char *argv[])
-
-
-{
-
-
-   char line[MAXLINE];
-   int found = 0;
-
-
-   if (argc != 2)
-	   printf("Usage: find pattern\n");
-
-
-   else
-	   while (get_line(line, MAXLINE) > 0)
-           if (strstr(line, argv[1]) != NULL) {
-			printf("%s", line);
-			found++;
-			
-		   }
-   return found;
-
-
-}
-
-int get_line (char s[], int lim)
-	
-{
-	int c, i;
-	
-	for (i = 0; i<lim-1 && (c=getchar()) != EOF && c!= '\n'; ++i)	
-		s[i] = c;
-	if (c == '\n') {
-		s[i] = c;
-		++i;
+    while(fgets(line, 512, fp) != NULL) {
+		if((strstr(line, tofind)) != NULL) {
+			printf("\n%s\n", line);
+		}
 	}
-	s[i] = '\0';
-	return i;
-	
+
+    fclose(fp);
 }
